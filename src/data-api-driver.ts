cd1c44ff65771a18b95f884caae1951c11adcbeb
Igal Klebanov <igalklebanov@gmail.com>
2022-12-10 17:49:23 +0200

    add `numAffectedRows` @ `DataApiConnection.executeQuery`.

--- src/data-api-driver.ts
+++ src/data-api-driver.ts
@@ -102,8 +102,14 @@ class DataApiConnection implements DatabaseConnection {
       })
       .promise();
     if (!r.columnMetadata) {
+      const numAffectedRows = BigInt(r.numberOfRecordsUpdated || 0)
+
       return {
-        numUpdatedOrDeletedRows: BigInt(r.numberOfRecordsUpdated || 0),
+        // @ts-ignore replaces `QueryResult.numUpdatedOrDeletedRows` in kysely > 0.22 
+        // following https://github.com/koskimas/kysely/pull/188
+        numAffectedRows,
+        // deprecated in kysely > 0.22, keep for backward compatibility.
+        numUpdatedOrDeletedRows: numAffectedRows,
         rows: [],
       };
     }
